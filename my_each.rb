1161cc7b5faeac6dad062915266922eb293253b8
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-06 14:40:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,9 +2,9 @@ collection = [1, 2, 3, 4]
 
 def my_each(collection)
   i=0 
-  while i+1<collection.length 
+  while i<collection.length 
   yield(collection[i])
-  i+=1 
+  i=i+1 
   end  
   collection
 end
